512763009930770dd706ab1d1e30df654bc99008
KMY <ionic023@gmail.com>
2023-01-17 13:39:06 +0900

    0으로 나누기 예외 처리, 출력 폰트 조절
    0으로 나누면 'Cannot divide by 0' 출력 후에 모든 값을 초기화.
    
    출력 값에 따른 폰트 조절이 일정하지 않았던 부분 수정

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@ import tkinter
 
 screen = tkinter.Tk()
 screen.title('CALCULATOR')
-screen.geometry('297x342')
+screen.geometry('297x342+100+100')
 screen.resizable(False, False)
 
 
@@ -15,8 +15,8 @@ def input(val):
 
     if type(val) == str:                            ##### operator part
         if len(calcValue[calindex]) > 13:  # automatically changes the font size
-            output_current.config(font=('D2Coding', 17))
-        else : output_current.config(font=('D2Coding', 30))
+            output_current.config(height=2, font=('D2Coding', 17))
+        # else : output_current.config(font=('D2Coding', 30))
 
         if not calcValue[0] : calcValue[0] = '0'
         if val == '.':                              ### decimal point part
@@ -74,32 +74,50 @@ def input(val):
                 del calcValue[len(calcValue) - 1]
                 for value in calcValue:
                     total += str(value)
-                calcValue[calindex] = eval(total)
+                calcValue[calindex] = str(eval(total))
+                if len(calcValue[0]) > 15 or len(calcValue[2]) > 15:
+                    output_history.config(height=1, font=('D2Coding', 8))
                 output_current.config(text=calcValue[calindex])
                 optFin = 1
             else :
-                calcValue.append(val)
-                output_history.config(text=(calcValue))
-                del calcValue[len(calcValue) - 1]
-                for value in calcValue:
-                    total += str(value)
-                calcValue[0] = eval(total)
-                calindex = 0
-                output_current.config(text=calcValue[calindex])
-                optFin = 1
+                if calcValue[1] == '/' and calcValue[2] == '0':
+                    calcValue[0] = '0'
+                    calcValue[1] = ''
+                    calcValue[2] = ''
+                    val = ''
+                    optFin = 1
+                    output_current.config(height=2, font=('D2Coding', 15))
+                    output_history.config(height=1, font=('D2Coding', 15), text='')
+                    calcValue[0] = 'Cannot divide by 0'
+                    # calindex = 0
+                else:
+                    calcValue.append(val)
+                    output_history.config(text=(calcValue))
+                    del calcValue[len(calcValue) - 1]
+                    for value in calcValue:
+                        total += str(value)
+                    calcValue[0] = str(eval(total))
+                    calindex = 0
+                    print(calcValue[calindex], len(calcValue[calindex]))
+                    if len(calcValue[0]) > 15 or len(calcValue[2]) > 15:
+                        output_history.config(height=1, font=('D2Coding', 8))
+                    output_current.config(text=calcValue[calindex])
+                    optFin = 1
             val = ''
 
         elif val == 'Back':
             if optFin == 1:
+                print("optFin is 1 my friend")
                 for i in range(len(calcValue)):
                     calcValue[i] = ''
                 output_history.config(text='')
                 calcValue[0] = '0'
                 optFin = 0
-                output_current.config(font=('D2Coding', 30))
+                output_history.config(height=1, font=('D2Coding', 15))
+                output_current.config(height=1, font=('D2Coding', 30))
             valSize = len(calcValue[calindex]) - 1
             calcValue[calindex] = calcValue[calindex][:valSize]
-            calcValue[0] = '0'
+            if calcValue[calindex] == '': calcValue[calindex] = '0'
             print('eraised', valSize)
             val = ''
 
@@ -109,8 +127,8 @@ def input(val):
             calcValue[2] = ''
             val = ''
             optFin = 0
-            output_current.config(font = ('D2Coding', 30))
-            output_history.config(text='')
+            output_current.config(height=1, font = ('D2Coding', 30))
+            output_history.config(height=1, font=('D2Coding', 15), text='')
 
         elif val == 'CE':
             if optFin == 1:
@@ -119,7 +137,8 @@ def input(val):
                 output_history.config(text='')
                 calcValue[0] = '0'
                 optFin = 0
-                output_current.config(font=('D2Coding', 30))
+                output_current.config(height=1, font=('D2Coding', 30))
+                output_history.config(height=1, font=('D2Coding', 15))
             calcValue[calindex] = '0'
             val = ''
 
@@ -127,10 +146,12 @@ def input(val):
 
 
     else:                                               ##### integer part
+        print("val in = ", val)
         if len(calcValue[calindex]) > 13:  # automatically changes the font size
-            output_current.config(height=1, font=('D2Coding', 17))
+            output_current.config(height=2, font=('D2Coding', 17))
             if len(calcValue[calindex]) > 20:
                 val = ''
+        else : output_current.config(height=1, font=('D2Coding', 30))
 
         if optFin == 1:
             calcValue[0] = '0'
@@ -139,13 +160,14 @@ def input(val):
             calindex = 0
             optFin = 0
             output_history.config(text='')
-            output_current.config(font=('D2Coding', 30))
-        if val != 0 and calcValue[calindex] == '0':    # prevent 0 in the value's head
+            output_current.config(height=1, font=('D2Coding', 30))
+            output_history.config(height=1, font=('D2Coding', 15))
+        if val != 0 and val != '' and calcValue[calindex] == '0':    # prevent 0 in the value's head
             calcValue[calindex] = calcValue[calindex].replace('0', '')
         if val == 0 and calcValue[calindex] == '0':    # prevent multiple 0 when the entire value is 0
-            print("HEY YOU")
             val = ''
             print(val)
+        print("val out = ", val)
 
 
     if not val == '': calcValue[calindex] += str(val)
@@ -169,13 +191,17 @@ btn = None
 calcValue = ['', '', '']
 calindex = len(calcValue) - 1
 optFin = 0
+crCount = 0
 
 # def create_button():
 #     button = []
+
 for row in range(mapper_size_row):
     for col in range(mapper_size_col):
         btn = tkinter.Button(screen, text='{}'.format(buttonmap[row][col]), height=2, width=8, bg='white', font=('D2Coding', '13'), command=lambda i = buttonmap[row][col]:input(i))
-        btn.bind('{}'.format(buttonmap[row][col]), lambda i = buttonmap[row][col]:input(i))
+        # if buttonmap[row][col] != 'CE' and buttonmap[row][col] != '-' :
+        #     screen.bind('<{}>'.format(buttonmap[row][col]), lambda i = buttonmap[row][col]: input(i))
+        # print(buttonmap[row][col])
         btn.grid(row=row+2, column=col)
 
 # create_button()
@@ -183,9 +209,9 @@ for row in range(mapper_size_row):
 
 
 #####           output part             #####
-output_history = tkinter.Label(screen, text=calcValue[calindex], font=('D2Coding', 15), anchor='se', fg='sky blue', bg='dark grey', width=10)
+output_history = tkinter.Label(screen, text=calcValue[calindex], font=('D2Coding', 15), anchor='se', fg='sky blue', bg='dark grey', height=1, width=10)
 output_history.grid(row=0, column=0, columnspan=4, sticky='e'+'w'+'s'+'n')
-output_current = tkinter.Label(screen, text='0', font=('D2Coding', 30), anchor='se', fg='white', bg='dark grey', width=10)
+output_current = tkinter.Label(screen, text='0', font=('D2Coding', 30), anchor='se', fg='white', bg='dark grey', height=1, width=10)
 output_current.grid(row=1, column=0, columnspan=4, sticky='e'+'w'+'s'+'n')
 
 
